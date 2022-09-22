3d368197226f0b5c0167ffe3ffc3e1e49718d3f8
Varsha <varshabhadana93@gmail.com>
2022-09-23 01:30:04 +0200

    Implemt checkbox functionality

--- src/App.js
+++ src/App.js
@@ -4,11 +4,21 @@ import { useEffect, useState } from 'react';
 function App() {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
-  const [attendingStatus, setAttendingStatus] = useState(false);
+
   const [guestList, setGuestList] = useState([]);
-  const [checkBoxValue, setCheckBoxValue] = useState(false);
-  const [isloading, setIsLoading] = useState(true);
+
+  const [isLoading, setIsLoading] = useState(true);
   const baseUrl = 'https://232afac8-d1ad-4b1c-a25c-b32515b1eaed.id.repl.co';
+
+  // fetching data array
+  useEffect(() => {
+    async function fetchGuest() {
+      const response = await fetch(`${baseUrl}/guests`);
+      const data = await response.json();
+      setGuestList(data);
+    }
+    fetchGuest();
+  }, []);
 
   // creating new guest
   async function addGuest() {
@@ -20,7 +30,6 @@ function App() {
       body: JSON.stringify({
         firstName: firstName,
         lastName: lastName,
-        attending: attendingStatus,
       }),
     });
     const createdGuest = await response.json();
@@ -30,44 +39,37 @@ function App() {
     const response = await fetch(`${baseUrl}/guests/${id}`, {
       method: 'DELETE',
     });
+
     const deletedGuest = await response.json();
+    console.log(deletedGuest);
   }
-  // fetching data array
-  useEffect(() => {
-    async function fetchGuest() {
-      const response = await fetch(`${baseUrl}/guests`);
-      const data = await response.json();
-      setGuestList(data);
-    }
-    fetchGuest();
-  }, []);
 
   // Updating a guest
-  async function updateGuest() {
-    const response = await fetch(`${baseUrl}/guests/1`, {
+  async function updateGuest(id, status) {
+    const response = await fetch(`${baseUrl}/guests/${id}`, {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ attending: true }),
+      body: JSON.stringify({ attending: status }),
     });
     const updatedGuest = await response.json();
   }
 
   // is loading msg
-  /* useEffect(() => {
-    if (guestList) {
+  useEffect(() => {
+    if (guestList.length > 0) {
       setIsLoading(false);
     }
   }, [guestList]);
-  if (isloading) return <div>Loading Text</div>; */
 
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-  /* if (!guestList) return <div>is Loading...</div>; */
+  if (isLoading) return <div>Loading Text</div>;
 
   return (
     <div>
       <h1>Guest List</h1>
+
+      {/* First name input */}
 
       <label htmlFor="First-name">First name</label>
       <input
@@ -76,6 +78,8 @@ function App() {
           setFirstName(event.target.value);
         }}
       />
+      {/* Last name input */}
+
       <label htmlFor="Last-name">Last name</label>
       <input
         value={lastName}
@@ -84,76 +88,47 @@ function App() {
         }}
       />
 
-      {/* checkbox created */}
-      {/* {guestList.map((el) => (
-        <div key={el.id}>
-          Attending Status
-          <input
-            checked={checkBoxValue}
-            type="checkbox"
-            aria-label="attending"
-            onChange={(event) => setCheckBoxValue(event.currentTarget.checked)}
-          />
-        </div>
-      ))} */}
-
-      {/* <input
-        checked={checkBoxValue}
-        type="checkbox"
-        aria-label="attending"
-        onChange={(event) => setCheckBoxValue(event.currentTarget.checked)}
-      /> */}
-
       {/* Button to add new guest */}
       <button onClick={addGuest}>Add</button>
-
-      {/* Button to  delete guest */}
-
-      {/*  <button
-        onClick={async () => {
-          await removeGuest();
-        }}
-      >
-        Remove
-      </button> */}
 
       {/* /* Mapping over an array */}
 
       <div>
-        <ul>
-          {
-            guestList.map((el) => (
-              <div
-                style={{
-                  display: 'flex',
-                  flexDirection: 'coloumn',
-                  marginright: '30px',
+        {
+          guestList.map((el) => (
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'coloumn',
+                marginright: '30px',
+              }}
+              key={el.id}
+            >
+              <p style={{ marginRight: '30px' }}>{el.firstName}</p>
+              <p style={{ marginRight: '30px' }}>{el.lastName}</p>
+
+              {/* Checkbox to change attending status */}
+              <input
+                checked={el.attending}
+                type="checkbox"
+                aria-label="attending"
+                onChange={async (event) => {
+                  await updateGuest(el.id, event.target.checked);
                 }}
-                key={el.id}
+              />
+              {/* Button to delete a guest */}
+              <button
+                onClick={async () => {
+                  await removeGuest(el.id);
+                }}
               >
-                <p style={{ marginRight: '30px' }}>{el.firstName}</p>
-                <p style={{ marginRight: '30px' }}>{el.lastName}</p>
-                <p>{String(el.attending)}</p>
-                <button
-                  onClick={async () => {
-                    await removeGuest(el.id);
-                  }}
-                >
-                  Remove
-                </button>
-                <input
-                  checked={checkBoxValue}
-                  type="checkbox"
-                  aria-label="attending"
-                  onChange={(event) =>
-                    setCheckBoxValue(event.currentTarget.checked)
-                  }
-                />
-              </div>
-            ))
-            /* .filter((index) => index !== key={el.id}) */
-          }
-        </ul>
+                Remove
+              </button>
+            </div>
+          ))
+
+          /* .filter((index) => index !== key={el.id}) */
+        }
       </div>
       {/*  {(guestList.filter((el , i) => i !== key={el.id}), [setGuesList])} */}
     </div>
